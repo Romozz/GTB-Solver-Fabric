1c2dc8c0ff153bbc85d20912e9dc866f441b1dca
Roman Lepekhin <romozrakal@gmail.com>
2025-08-20 14:26:14 +0300

    Update CharSlotWidget.java

--- src/main/java/com/romoz/gtb/ui/CharSlotWidget.java
+++ src/main/java/com/romoz/gtb/ui/CharSlotWidget.java
@@ -3,8 +3,8 @@ package com.romoz.gtb.ui;
 import com.romoz.gtb.GTBHelper;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.gui.screen.narration.NarrationMessageBuilder; // <-- правильный импорт для 1.21.x
 import net.minecraft.client.gui.widget.ClickableWidget;
-import net.minecraft.client.gui.widget.NarrationMessageBuilder;
 import net.minecraft.text.Text;
 import org.lwjgl.glfw.GLFW;
 
@@ -100,7 +100,7 @@ public class CharSlotWidget extends ClickableWidget {
 
     @Override
     protected void appendClickableNarrations(NarrationMessageBuilder builder) {
-        // не озвучиваем
+        // Ничего не озвучиваем
     }
 
     public void refreshFromState() {
